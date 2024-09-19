6c1cf5c64006d7cefe3ef2c917c5d43b75f39b7a
Lucas Balen Mattos <balenmattoslucas12@gmail.com>
2024-09-19 14:08:37 -0300

    Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,43 +1,48 @@
-class Calculadora():
-def __init__ (self, num1, num2):
-self.num1 = num1
-self.num2 = num2
+class calculadora():
+        def _init_(self,num1,num2):
+                self.num1 = num1 
+                self.num2 = num2 
+        def soma(self):
+                return self.num1 + self.num2 
+        def subtraçãoo(self):
+                return self.num1 - self.num2
+        def multiplicação(self):
+                return self.num1 * self.num2
+        def divisão(self):
+                return self.num1 / self.num2
+        def exponenciação(self):
+                return self.num1 ** self.num2
+        def divisão_interia(self):
+                return self.num1 // self.num2
+        def modulo(self):
+                return self.num1 % self.num2
+        
+num1 = int(input("Digite o seu primeiro número:  "))
+num2 = int(input("Digite o segundo número:  "))
+operação = calculadora(num1,num2)
 
-def soma(self):
-    return self.num1 + self.num2
-def subtracao(self):
-    return self.num1 - self.num2
-def multiplicacao(self):
-    return self.num1 * self.num2
-def divisao(self):
-    return self.num1 / self.num2
-def exponenciacao(self):
-    return self.num1 ** self.num2
-def divisao_inteira(self):
-    return self.num1 // self.num2
-def modulo(self):
-    return self.num1 % self.num2
+print('escolha 1 para SOMA')
+print('escolha 2 para SUBTRAÇÃO')
+print('escolha 3 para MULTIPLICAÇÃO')
+print('escolha 4 para DIVISÃO')
+print('escolha 5 para ESPONENCIAÇÃO')
+print('escolha 6 para DIVISÃO INTEIRA')
+print('escolha 7 para MODULO')
+escolha = int(input('Digite o número da operação'))
 
-num1 = int(input("Digite o primeiro número: "))
-num2 = int(input("Digite o segundo número: "))
-operacao = Calculadora(num1, num2)
-
-print("Escolha 1 para SOMA")
-print("Escolha 2 para SUBTRAÇÃO")
-print("Escolha 3 para DIVISÃO")
-print("Escolha 4 para EXPONENCIAÇÃO")
-print("Escolha 5 para DIVISÃO INTEIRA")
-print("Escolha 7 para MÓDULO")]
-escolha = int(input('Digite o número da operação: '))
-if escolha == 1:
-    print(f"O resultado da soma entre {num1} e {num2} é: ", operacao.soma())
-elif escolha == 2:
-    print(f"O resultado da subtração entre {num1} e {num2} é: ", operacao.subtracao())
-elif escolha == 3:
-    print(f"O resultado da divisão entre {num1} e {num2} é: ", operacao.divisao())
-elif escolha == 4:
-    print(f"O resultado da exponencição entre {num1} e {num2} é: ", operacao.exponenciacao())
-elif escolha == 5:
-    print(f"O resultado da divisão inteira entre {num1} e {num2} é: ", operacao.divisao_inteira())
-elif escolha == 5:
-    print(f"O resultado da módulo entre {num1} e {num2} é: ", operacao.modulo())
+if escolha == 1 :
+        print('O resultado da soma entre {num1} e {num2} é: ', operação.soma())
+elif escolha == 2 :
+        print('O resultado da subtração entre {num1} e {num2} é: ', operação.subtraçãoo())
+elif escolha == 3 :
+        print('O resultado da multiplicação entre {num1} e {num2} é: ', operação.multiplicação())
+elif escolha == 4 :
+        print('O resultado da divisão entre {num1} e {num2} é: ', operação.divisão())
+elif escolha == 5 :
+        print('O resultado da exponenciação entre {num1} e {num2} é: ', operação.exponenciação())
+elif escolha == 6 :
+        print('O resultado da divisão inteira entre {num1} e {num2} é: ', operação.divisão_interia())
+elif escolha == 7 :
+        print('O resultado do modulo entre {num1} e {num2} é: ', operação.modulo())
+else:
+        print ('Operação invalida')
