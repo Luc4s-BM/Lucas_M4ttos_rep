bac9fa3b2ea6568484e81f820792d70854a77ffe
Lucas Balen Mattos <balenmattoslucas12@gmail.com>
2024-09-26 15:09:58 -0300

    COMMIT 2609

---
+++ exercicios_vscode/Exercicios_vscode_2609.py
@@ -0,0 +1,28 @@
+#1
+#print("Digite dois números")
+
+#num1 = int(input("Digite o primeiro valor: "))
+#num2 = int(input("Digite o segundo valor: "))
+#soma = num1 + num2
+#subtracao = num1 - num2
+#multiplicacao = num1 * num2
+#divisao = num1 / num2
+#print(f"A soma: : {soma} - A subtração: {subtracao} - A multiplicação: {multiplicacao} A divisão: {divisao}")
+
+#2
+#print("Vamos calcular seu IMC: ")
+#peso = float(input("Digite seu peso (Obs: Escreva em quilos):"))
+#altura = float(input("Digite sua altura (Obs: Digite em metros):"))
+#imc = peso / (altura * altura)
+#print(f"O resultado do seu IMC: {imc}")
+
+#3
+#frase = "Bem vindo turma da Programação II ao mundo da programação Python!!!" [:: - 1 ]
+#print(frase)
+
+#4 
+
+#5
+import datetime
+agora = datetime.datetime.now()
+print(agora)
