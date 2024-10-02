1b22f514cfe3e8796c3fded66f440281c2582539
Lucas Balen Mattos <balenmattoslucas12@gmail.com>
2024-10-02 17:05:19 -0300

    COMMIT 0210

--- exercicios_vscode/Exercicios_vscode_2609.py
+++ exercicios_vscode/Exercicios_vscode_2609.py
@@ -14,7 +14,27 @@ print("Vamos calcular seu IMC: ")
 peso = float(input("Digite seu peso (Obs: Escreva em quilos):"))
 altura = float(input("Digite sua altura (Obs: Digite em metros):"))
 imc = peso / (altura * altura)
-print(f"O resultado do seu IMC: {imc}")
+print(f"O resultado do seu IMC: {imc:.2f}")
+if imc > 40:
+    print("Você está muito obeso!")
+    
+elif imc < 18.5:
+    print("Você está muito magro!")
+    
+elif imc > 18.5 and imc < 24.9:
+    print("Você está no peso normal!")
+    
+elif imc > 25 and imc < 29.9:
+    print("Você está com excesso de peso!")
+    
+elif imc > 30 and imc < 34.9:
+    print("Você está com obesidade classe I!")
+    
+elif imc > 35 and imc < 39.9 :
+    print("Você está com obesidade classe II!")
+    
+elif imc > 40:
+    print("Você está com obesidade classe III!")  
 
 #3
 frase = "Bem vindo turma da Programação II ao mundo da programação Python!!!" [:: - 1 ]
@@ -27,8 +47,7 @@ uuid_gerador_senha = '35b35f68-9687-4ca6-bb79-f59201d40b0739e5b006-f4ab-49cb-a07
 senha_gerada = "  ".join(random.sample(uuid_gerador_senha, tamanho_da_senha_a_ser_gerada))
 print(f"Senha gerada com {tamanho_da_senha_a_ser_gerada} caracteres: {senha_gerada}")
 
-
 #5
 import datetime
 agora = datetime.datetime.now()
-print(agora)
+print(agora).strftime("%d/%m/%Y  %H:%M:%S")
