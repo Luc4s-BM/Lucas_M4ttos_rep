e04e73c8d9635576b2c6f749f8c37fecfa86b277
Lucas Balen Mattos <balenmattoslucas12@gmail.com>
2024-11-13 17:25:11 -0300

    COMMIT 1311

--- funções_python/Tratamento_string/funções_string.py
+++ funções_python/Tratamento_string/funções_string.py
@@ -6,14 +6,13 @@ def calcular_tamanho_string(caracteres_da_string):
     else:    
         return - 1 
     
-letra_da_musica = input("Digite a String: ").strip()
+letra_da_musica = ("Meu caminho é cada manhã Não procure saber onde vou Meu destino não é de ninguém Eu não deixo os meus passos no chão Se você não entende, não vê Se não me vê, não entende Não procure saber onde estou Se o meu jeito te surpreende Se o meu corpo virasse sol Minha mente virasse sol Mas só chove e chove Chove e chove Se um dia eu pudesse ver Meu passado inteiro E fizesse parar de chover Nos primeiros erros, oh O meu corpo viraria sol Minha mente viraria... Mas só chove e chove Chove e chove Se um dia e pudesse ver Meu passado inteiro E fizesse parar de chover Nos primeiros erros, oh O meu corpo viraria sol Minha mente viraria... Mas só chove e chove Chove e chove, oh O meu corpo viraria sol Minha mente viraria sol Mas só chove e chove Chove e chove, oh Chove e chove, oh Chove e chove, oh Chove e chove").strip()
 tamanho_da_string_inserida = calcular_tamanho_string(letra_da_musica)
 
 if tamanho_da_string_inserida <= 0: 
     print("Entrada inválida!")
 else:
-    print(tamanho_da_string_inserida)
-    print(f'A String tem {tamanho_da_string_inserida} caracteres.')
+    print(f'A música "Primeiros Erros" tem {tamanho_da_string_inserida} caracteres.')
 
 
 
