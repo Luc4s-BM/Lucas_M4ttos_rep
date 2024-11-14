b599eb6108451acbf7fd6a434925c6ed45b28ac7
Lucas Balen Mattos <balenmattoslucas12@gmail.com>
2024-11-14 15:10:54 -0300

    COMMIT 1411

--- funções_python/Tratamento_string/funções_string.py
+++ funções_python/Tratamento_string/funções_string.py
@@ -14,5 +14,78 @@ if tamanho_da_string_inserida <= 0:
 else:
     print(f'A música "Primeiros Erros" tem {tamanho_da_string_inserida} caracteres.')
 
+#2
+def pegar_caracteres_da_string(lista_dos_caracteres, posicao_dos_caracteres):
+    tamanho_da_string_passada = len(lista_dos_caracteres)
+    
+    if posicao_dos_caracteres >= tamanho_da_string_passada or posicao_dos_caracteres < 0:
+        return -1 
+    else:
+        return lista_dos_caracteres[posicao_dos_caracteres]  # Retorna o caractere na posição
+
+string_passada = input("Digite uma frase qualquer:\n")
+indice_acessado_string = input("Informe um índice numérico a ser acessado na frase (Começará do zero): ")
+
+if not indice_acessado_string.isdigit():
+    print("Por favor, informe um índice válido!")
+elif int(indice_acessado_string) < 0:
+    print("Por favor, informe um índice positivo ou ZERO!")
+else:
+    indice_acessado_string = int(indice_acessado_string)
+    caractere = pegar_caracteres_da_string(string_passada, indice_acessado_string)
+    
+    if caractere == -1:
+        print(f"A posição {indice_acessado_string} não existe na string \"{string_passada}\" \n")
+        for indice_caracter in enumerate(len(string_passada)):
+            print(f"No índice {indice_caracter} temos o caractere '{string_passada[indice_caracter]}'")
+    else:
+        print(f"Na {indice_acessado_string}ª posição da string \"{string_passada}\" temos o caractere '{caractere}'")
+        
+#3
+def exibir_caracteres_e_posicoes(string):
+    for indice, caracter in enumerate(string):
+        print(f"{caracter} - {indice + 1}º caracter da String")
+string_passada = input("Digite uma frase qualquer:\n")
+exibir_caracteres_e_posicoes(string_passada)
+
+#4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
