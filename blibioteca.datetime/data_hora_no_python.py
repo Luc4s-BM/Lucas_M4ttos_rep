675cba4fbafabc7a60dfcc4802e3c5d11eda63b4
Lucas Balen Mattos <balenmattoslucas12@gmail.com>
2024-11-27 17:28:40 -0300

    COMMIT 2711

--- blibioteca.datetime/data_hora_no_python.py
+++ blibioteca.datetime/data_hora_no_python.py
@@ -1,3 +1,4 @@
+1#
 """
 Pytz é uma bilioteca do Python que permite calular fusoshorários e
 resolver problemas com horários ambiguos no final do horário de verão.
@@ -7,3 +8,42 @@ IANA. Ele é principalmente usado pelo módulo "zoneinfo" da blioteca padrão,
 mas também pode ser usado como fonte de dados de fuso horário para outras
 bibliotecas de fuso horário.
 """
+
+#2
+from datetime import datetime, date, time, timedelta
+
+print(datetime.now())
+
+
+#3
+import datetime
+hora_de_agora = datetime.datetime.now()
+print(hora_de_agora.strftime("%d/%m/%Y"))
+
+#OU
+from datetime import datetime, date
+print(date.today())
+
+
+#4
+from datetime import datetime, time
+horario_espeifico = time(14, 30, 59)
+print(horario_espeifico)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
