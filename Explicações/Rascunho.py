3e5b5a822ad0bb9e9892421d428e6038d6b4496d
Lucas Balen Mattos <balenmattoslucas12@gmail.com>
2024-12-04 19:43:10 -0300

    Add files via upload

---
+++ Explicações/Rascunho.py
@@ -0,0 +1,50 @@
+"""frase = input("Digite sua frase: ")
+
+def quantidade_de_letras(letras):
+    return len(letras)
+
+quantidade = quantidade_de_letras(frase)
+print(f"A quantidade de caracteres na frase é: {quantidade}")
+
+nome = "LucasMattos"
+print(nome.find("a"))
+print(nome.upper())
+print(nome.lower())
+print(nome.isdigit())
+print(nome.isalpha())
+print(nome.count("a"))
+
+nome1 = input("Digite seu nome: ")
+print("Olá "+ nome1)
+
+x = 1
+try:
+    print(x)
+except:
+    print("An exception occurred")
+
+try:
+    print("Hello")
+except:
+    print("Something went wrong")
+else:
+    print("Nothing went wrong")
+
+frase = "Hello World"
+corte = frase.split()
+print(corte)
+"""
+num1 = 3
+if not num1<4:
+    print("ok")
+else:
+    print("Not ok")
+
+
+texto = "python é muito chato"
+print(texto.replace(" ",""))
+
+
+print("Welcome to", end = ' ')
+print("GeeksforGeeks", end= ' ')
+print("Fodase")
