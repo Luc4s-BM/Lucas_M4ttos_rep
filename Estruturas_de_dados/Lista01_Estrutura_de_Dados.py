d7d0803d4e74012654e1e0eb6d42644dc9f28c76
Lucas Balen Mattos <balenmattoslucas12@gmail.com>
2024-12-04 23:30:21 -0200

    COMMIT 0412

---
+++ Estruturas_de_dados/Lista01_Estrutura_de_Dados.py
@@ -0,0 +1,30 @@
+#1
+dias_da_semana = ("Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabádo", "Domingo")
+meses_do_ano = ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
+estacoes_do_ano =  ("Verão", "Outono"," Inverno", "Primavera")
+data = (dias_da_semana, meses_do_ano, estacoes_do_ano)
+def main(tupla):
+    print("Dias da Semana: ", tupla[0])
+    print("Meses do Ano: ", tupla[1])
+    print("Estações do Ano: ", tupla[2])
+print("Data Tipo Tupla:")
+main(data)
+
+#2
+dias_da_semana2 = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabádo", "Domingo"]
+meses_do_ano2 = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+estacoes_do_ano2 = ["Verão", "Outono"," Inverno", "Primavera"]
+data2 = [dias_da_semana2, meses_do_ano2, estacoes_do_ano2]
+def main2(lista):
+    print("Dias da Semana: ", lista[0])
+    print("Meses do Ano: ", lista[1])
+    print("Estações do Ano: ", lista[2])
+print("Data Tipo Lista:")   
+main2(data2)
+
+#3
+
+
+
+
+
